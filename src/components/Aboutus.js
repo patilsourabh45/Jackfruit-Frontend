011cd6b63521e0220f2476c280af23096215782f
Sourabh Patil <patilsourabh45@gmail.com>
2021-09-12 21:38:08 +0530

    Error fixed

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -3,16 +3,18 @@ import React from 'react';
 function Aboutus() {
     return (
         <>
-        <h1 style={{textAlign:"center"}}>Aboutus</h1>
-        <p>WHAT IS JACKFRUIT?</p>
-        <p>A platform that simplifies Personal Finance for Indians with a simple, guided journey</p>
-        <br></br>
-        <p>WHAT CAN YOU DO WITH US?</p>
-        <p>Understand your tax liability and how to save more tax by using the Jackfruit app</p>
-        <p>Finance doesn’t have to be so complicated! Learn about managing your money from a layman's perspective on the Jackfruit app</p>
-        <p>Understand what insurance products are best for you, and get the right policy for yourself!
+            <h1 style={{ textAlign: "center" }}>Aboutus</h1>
+            <div style={{marginLeft:'15px'}}>
+                <p>WHAT IS JACKFRUIT?</p>
+                <p>A platform that simplifies Personal Finance for Indians with a simple, guided journey</p>
+                <br></br>
+                <p>WHAT CAN YOU DO WITH US?</p>
+                <p>Understand your tax liability and how to save more tax by using the Jackfruit app</p>
+                <p>Finance doesn’t have to be so complicated! Learn about managing your money from a layman's perspective on the Jackfruit app</p>
+                <p>Understand what insurance products are best for you, and get the right policy for yourself!
 
-</p>
+                </p>
+            </div>
         </>
     )
 }
